a9fef7a5c87378eb3f04877cfadba821f93c9315
anas <anastah3456@gmail.com>
2024-05-22 02:53:15 +0100

    Added some basic check conditions in the Facility Controller.

--- facility-service/src/main/java/com/sportscape/api/facilityservice/model/Location.java
+++ facility-service/src/main/java/com/sportscape/api/facilityservice/model/Location.java
@@ -14,8 +14,13 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="Location_city")
     private String city;
+
+    @Column(name="Postal_code")
     private String postalCode;
+
+    @Column(name="Location_coordinates")
     private String coordinates;
 
     @ManyToMany(mappedBy = "locations")
