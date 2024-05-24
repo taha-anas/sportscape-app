2e13ec4a23a21274b0fcd9c6e2c2246137c71291
anas <anastah3456@gmail.com>
2024-05-24 04:33:04 +0100

    Added Login and Signup and SportFacilities Interfaces

--- SportScape-Front/sportscape-app/src/app/app.routes.ts
+++ SportScape-Front/sportscape-app/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {SignupComponent} from "./signup/signup.component";
+import {SportFacilitiesComponent} from "./sport-facilities/sport-facilities.component";
+
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'dashboard',
-  component: DashboardComponent, pathMatch:'full'
+  component: DashboardComponent,children: [{ path: 'sport-facilities',
+      component: SportFacilitiesComponent }
+    ]
 }
     ];
--- SportScape-Front/sportscape-app/src/app/login/login.component.ts
+++ SportScape-Front/sportscape-app/src/app/login/login.component.ts
@@ -1,29 +1,45 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import {AuthRequest} from "../model/models";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule]
+  imports: [FormsModule, CommonModule, HttpClientModule, RouterLink]
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  error: string = '';
+  authRequest : AuthRequest ={email: "",password: ""};
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   onSubmit() {
     if (this.email && this.password) {
-      // Perform login logic here
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
-
-      // Navigate to another route on successful login
-      this.router.navigate(['/dashboard']);
+      this.authenticateUser();
+    } else {
+      this.error = "Please enter both email and password.";
     }
+  }
+
+  private authenticateUser() {
+    this.authRequest= { email: this.email, password: this.password };
+    this.http.post<{token: string}>('http://localhost:8080/api/users/api/auth', this.authRequest)
+      .subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          this.router.navigate(['/dashboard/sport-facilities']);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+          this.error = "Authentication failed. Check your credentials.";
+        }
+      });
   }
 }
